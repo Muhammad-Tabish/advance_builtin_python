eacba9d249b32aac1c0065daf4d759d9688b06a5
tabishhameed <tabishhameed34@gmail.com>
2021-09-18 23:28:02 +0800

    first hundred generators

---
+++ classesgenerator.py
@@ -0,0 +1,35 @@
+class firsthundredgenerator:
+    def __init__(self):
+        self.number = 0
+    def __next__(self):
+        if self.number < 100:
+            current = self.number
+            self.number += 1
+            return current
+        else:
+            raise StopIteration()
+my_gen = firsthundredgenerator()
+
+print(next(my_gen))
+print(next(my_gen))
+print(next(my_gen))
+print(next(my_gen))
+
+
+class firstfiveiterator:
+    def __init__(self):
+        self.numbers = [1, 2, 3, 4, 5]
+        self.i = 0
+    def __next__(self):
+        if self.i < len(self.numbers):
+            current = self.numbers[self.i]
+            self.i+=1
+            return current
+        else:
+            raise StopIteration()
+
+fivenuber = firstfiveiterator()
+print(next(fivenuber))
+print(next(fivenuber))
+
+
